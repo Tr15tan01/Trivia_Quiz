2224d6a1d54c733a793a38876f5d37a72dbb4ba6
titevar <59051464+titevar@users.noreply.github.com>
2021-10-10 00:35:35 +0400

    added functionality

--- js.js
+++ js.js
@@ -4,6 +4,15 @@ const api = fetch('https://opentdb.com/api.php?amount=3&category=28&type=multipl
         //quesion number
         let i = 0;
 
+        //next question
+        const nextQuestion = () => {
+            i = i + 1
+            loadQuestion()
+        }
+
+
+
+
         const loadQuestion = () => {
             //question assign to element
             const question = document.getElementById('q1');
@@ -16,15 +25,30 @@ const api = fetch('https://opentdb.com/api.php?amount=3&category=28&type=multipl
             const answersContainer = document.getElementById('answers')
             answersContainer.innerHTML = answers.map(answer => `<div class = 'answer'>${answer}</div>`).join("<br>")
 
-        }
+            const selectAnswer = () => {
 
+                const checkAnswer = (e) => {
+                    console.log(e.target.textContent)
+                    if (e.target.textContent == correct_answer) {
+                        console.log('tadaa')
+                    } else {
+                        console.log('fuck')
+                    }
+                }
+
+                const answerToSelect = document.querySelectorAll('div.answer')
+                // console.log(answerToSelect)
+                for (let j = 0; j < answerToSelect.length; j++) {
+                    answerToSelect[j].addEventListener('click', checkAnswer)
+                }
+
+            }
+
+            selectAnswer()
+
+        }
+        // initially load first question
         loadQuestion()
-
-        const nextQuestion = () => {
-            i = i + 1
-            loadQuestion()
-        }
-
 
 
         // console.log(data)
