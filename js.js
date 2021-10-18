6ee49dc70f47cf71396b6a7b515ea3c7441cf821
titevar <59051464+titevar@users.noreply.github.com>
2021-10-18 20:44:43 +0400

    added levels and score

--- js.js
+++ js.js
@@ -1,104 +1,132 @@
-fetch('https://opentdb.com/api.php?amount=3&category=28&difficulty=easy&type=multiple')
-    .then(response => response.json())
-    .then(data => {
-        //quesion number
-        let i = 0;
-        let score = 0;
-        let selected = false;
-        const nextButton = document.getElementById('nextButton')
-
-        //score display
-        const scoreDisplay = document.getElementById('score')
-        scoreDisplay.textContent = score;
-
-        function shuffleArray(array) {
-            for (let i = array.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]];
-            }
-        }
-
-        const loadQuestion = () => {
-            //question assign to element
-            const question = document.getElementById('question');
-            question.innerHTML = data.results[i].question
-            const incorrect_answers = data.results[i].incorrect_answers;
-            const correct_answer = data.results[i].correct_answer;
-            // const answers = incorrect_answers.concat(correct_answer)
-            const answers = [correct_answer, ...incorrect_answers]
-            shuffleArray(answers)
-            // console.log('answers', answers)
-            nextButton.disabled = true;
-            const answersContainer = document.getElementById('answers')
-            answersContainer.innerHTML = answers.map(answer => `<div class = 'answer'>${answer}</div>`).join("<br>")
-
-            const selectAnswer = () => {
-
-                const checkAnswer = (e) => {
-                    console.log(e.target.textContent)
-                    nextButton.disabled = false;
-                    if (e.target.textContent == correct_answer) {
-                        console.log('tadaa')
-                        e.target.classList.toggle('correct')
-                        score = score + 1;
-                        console.log(score, 'score')
-                        scoreDisplay.textContent = score;
-                    } else {
-                        console.log('fuck')
-                        e.target.classList.toggle('incorrect')
-
-                    }
-                    for (let j = 0; j < answerToSelect.length; j++) {
-                        answerToSelect[j].removeEventListener('click', checkAnswer)
-                        answerToSelect[j].classList.add('noHover')
-                    }
-
-                }
-
-                const answerToSelect = document.querySelectorAll('div.answer')
-                // console.log(answerToSelect)
-                for (let j = 0; j < answerToSelect.length; j++) {
-                    answerToSelect[j].addEventListener('click', checkAnswer)
-                }
-
-            }
-
-            selectAnswer()
-
-        }
+// let difficulty
 
 
-        // initially load first question
-        loadQuestion()
-
-
-        //next question
-        const nextQuestion = () => {
-            if (i < 2) {
-                console.log('with if i is ', i)
-                i = i + 1
-                console.log(selected)
-                loadQuestion()
-            }
-            else {
-                console.log('finish')
-                const containerDiv = document.getElementsByClassName('container')[0]
-                const finishedDiv = document.getElementById('finished')
-                const finalScore = document.getElementById('finalScore')
-                finishedDiv.style.display = 'block';
-                containerDiv.style.display = 'none';
-                finalScore.textContent = score;
-            }
-
-        }
-
-
-        nextButton.addEventListener('click', nextQuestion)
-    })
-
-//starter function
-const start = () => {
+const start = (difficulty = 'easy') => {
     const divToHide = document.getElementsByClassName('startQuiz')[0];
     divToHide.style.display = 'none'
-    console.log('start')
+    console.log('difficulty', difficulty)
+    fetcher(difficulty)
 }
+
+const fetcher = (difficulty) => {
+    console.log('difficulty fetcher', difficulty)
+    fetch(`https://opentdb.com/api.php?amount=3&category=28&difficulty=${difficulty}&type=multiple`)
+        .then(response => response.json())
+        .then(data => {
+            //quesion number
+            let i = 0;
+            let score = 0;
+            let selected = false;
+            const nextButton = document.getElementById('nextButton')
+
+            const finishedDiv = document.getElementById('finished')
+            const containerDiv = document.getElementsByClassName('container')[0]
+
+
+            //score display
+            const scoreDisplay = document.getElementById('score')
+            scoreDisplay.textContent = score;
+
+            function shuffleArray(array) {
+                for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                }
+            }
+
+            const loadQuestion = () => {
+                //question assign to element
+                const question = document.getElementById('question');
+                question.innerHTML = data.results[i].question
+                const incorrect_answers = data.results[i].incorrect_answers;
+                const correct_answer = data.results[i].correct_answer;
+                // const answers = incorrect_answers.concat(correct_answer)
+                const answers = [correct_answer, ...incorrect_answers]
+                shuffleArray(answers)
+                // console.log('answers', answers)
+                nextButton.disabled = true;
+                const answersContainer = document.getElementById('answers')
+                answersContainer.innerHTML = answers.map(answer => `<div class = 'answer'>${answer}</div>`).join("<br>")
+                const selectAnswer = () => {
+
+                    const checkAnswer = (e) => {
+                        // console.log(e.target.textContent)
+                        nextButton.disabled = false;
+                        if (e.target.textContent == correct_answer) {
+                            console.log('tadaa')
+                            e.target.classList.toggle('correct')
+                            score = score + 1;
+                            // console.log(score, 'score')
+                            scoreDisplay.textContent = score;
+                        } else {
+                            // console.log('fuck')
+                            e.target.classList.toggle('incorrect')
+
+                        }
+                        for (let j = 0; j < answerToSelect.length; j++) {
+                            answerToSelect[j].removeEventListener('click', checkAnswer)
+                            answerToSelect[j].classList.add('noHover')
+                        }
+
+                    }
+
+                    const answerToSelect = document.querySelectorAll('div.answer')
+                    // console.log(answerToSelect)
+                    for (let j = 0; j < answerToSelect.length; j++) {
+                        answerToSelect[j].addEventListener('click', checkAnswer)
+                    }
+
+                }
+
+                selectAnswer()
+
+            }
+
+
+            // initially load first question
+            loadQuestion()
+
+
+            //next question
+            const nextQuestion = () => {
+                if (i < data.results.length - 1) {
+                    i = i + 1
+                    loadQuestion()
+                    console.log('i = ', i)
+                    console.log('data results', data.results.len)
+                }
+                else {
+                    console.log('finish')
+                    // const containerDiv = document.getElementsByClassName('container')[0]
+                    // const finishedDiv = document.getElementById('finished')
+                    const finalScore = document.getElementById('finalScore')
+                    finishedDiv.style.display = 'block';
+                    containerDiv.style.display = 'none';
+                    finalScore.textContent = score;
+                    console.log('finalScore', score)
+                }
+
+                console.log('the score id', score)
+
+            }
+
+            nextButton.addEventListener('click', nextQuestion)
+
+            const nextLevel = () => {
+                difficulty = 'medium'
+                start('medium')
+                finishedDiv.style.display = 'none'
+                containerDiv.style.display = 'block'
+            }
+
+            const nextLevelButton = document.getElementById('nextLevelButton')
+            nextLevelButton.addEventListener('click', nextLevel)
+        })
+}
+
+//starter function
+// const start = () => {
+//     const divToHide = document.getElementsByClassName('startQuiz')[0];
+//     divToHide.style.display = 'none'
+// }
+
