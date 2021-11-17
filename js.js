ced198e4214d3c0d670eeb68ee55de0e64187d0c
titevar <59051464+titevar@users.noreply.github.com>
2021-11-18 03:58:07 +0400

    removed console.logs increased to 12 questions

--- js.js
+++ js.js
@@ -5,16 +5,14 @@ window.start = () => {
     const starterDiv = document.getElementsByClassName('startQuiz')[0];
     // const spinner = document.getElementById('spinner')
     starterDiv.style.display = 'none'
-    console.log('difficulty - start function')
     fetcher()
 }
 
 const fetcher = () => {
-    console.log('difficulty fetcher')
     const spinner = document.getElementById('spinner')
     const containerDiv = document.getElementsByClassName('container')[0]
     containerDiv.style.display = 'none'
-    fetch(`https://opentdb.com/api.php?amount=3&category=28&type=multiple`)
+    fetch(`https://opentdb.com/api.php?amount=12&category=28&type=multiple`)
         .then(response => response.json())
         .then(data => {
             //quesion number
@@ -32,8 +30,7 @@ const fetcher = () => {
             const scoreDisplay = document.getElementById('score')
             scoreDisplay.textContent = `Your score is: ${score}`;
 
-            function shuffleArray(array) {
-                console.log('array shuffled')
+            const shuffleArray = (array) => {
                 for (let i = array.length - 1; i > 0; i--) {
                     const j = Math.floor(Math.random() * (i + 1));
                     [array[i], array[j]] = [array[j], array[i]];
@@ -42,7 +39,6 @@ const fetcher = () => {
 
             const loadQuestion = () => {
                 //question assign to element
-                console.log('%cloadQuestion function started', redLogStyle)
                 const question = document.getElementById('question');
                 question.innerHTML = data.results[i].question
                 const incorrect_answers = data.results[i].incorrect_answers;
@@ -50,21 +46,18 @@ const fetcher = () => {
                 // const answers = incorrect_answers.concat(correct_answer)
                 const answers = [correct_answer, ...incorrect_answers]
                 shuffleArray(answers)
-                // console.log('answers', answers)
                 nextButton.disabled = true;
                 const answersContainer = document.getElementById('answers')
                 answersContainer.innerHTML = answers.map(answer => `<div class = 'answer'>${answer}</div>`).join("<br>")
                 const selectAnswer = () => {
 
                     const checkAnswer = (e) => {
-                        // console.log(e.target.textContent)
-                        console.log('%ccheckAnswer function', greenLogStyle)
                         nextButton.disabled = false;
                         if (e.target.textContent == correct_answer) {
-                            // console.log('answer')
+
                             e.target.classList.toggle('correct')
                             score = score + 1;
-                            // console.log(score, 'score')
+
                             scoreDisplay.textContent = `Your score is: ${score}`;
                         } else {
                             // console.log('fuck')
@@ -79,7 +72,6 @@ const fetcher = () => {
                     }
 
                     const answerToSelect = document.querySelectorAll('div.answer')
-                    // console.log(answerToSelect)
                     for (let j = 0; j < answerToSelect.length; j++) {
                         answerToSelect[j].addEventListener('click', checkAnswer)
                     }
@@ -97,25 +89,18 @@ const fetcher = () => {
 
             //next question
             const nextQuestion = () => {
-                console.log('next question function')
                 if (i < data.results.length - 1) {
                     i = i + 1
                     loadQuestion()
-                    console.log('i = ', i)
-                    console.log('data results length', data.results.length)
                 }
                 else {
-                    console.warn('finished')
                     // const containerDiv = document.getElementsByClassName('container')[0]
                     // const finishedDiv = document.getElementById('finished')
                     const finalScore = document.getElementById('finalScore')
                     finishedDiv.style.display = 'block';
                     containerDiv.style.display = 'none';
                     finalScore.textContent = `Your score is: ${score}`;
-                    console.log('%cfinalScore', blueLogStyle, score)
                 }
-
-                console.log('the score is -', score)
 
             }
 
